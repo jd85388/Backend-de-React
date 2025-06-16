ecc59da39244ff08a940b07f4322c19d36af660c
jeferson <jd8538879@gmail.com>
2025-06-16 18:28:33 -0500

    se sube la carpeta de backend

---
+++ dist/src/config/config.js
@@ -0,0 +1,10 @@
+export const dbConfig = {
+    user: 'admin123',
+    password: 'lifeReminder001',
+    server: "lifereminderpruebas.database.windows.net",
+    database: 'Life_Reminder',
+    option: {
+        encrypt: true,
+        enableArithAbort: true
+    }
+};
